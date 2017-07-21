686b497f9723bdc827ba1a3e58d7466dee1b4365
Tim Zwolak <Tim.Zwolak@charter.com>
2017-07-21 16:29:31 -0600

    add category dropdown to videos

--- web/models/video.ex
+++ web/models/video.ex
@@ -16,7 +16,7 @@ defmodule Rumbl.Video do
   """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:url, :title, :description])
+    |> cast(params, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
   end
 end
